4acb029c7e92081701bd1d24d51e0b9d143187ab
sluxa <straeter.luca@gmail.com>
2024-03-19 14:08:57 +0100

    Refactor code to convert TypeScript to JavaScript
    Renamed and refactored 'FetchrInstance.ts' into JavaScript in order to better facilitate integration with projects that do not use TypeScript. As a part of this change, 'tsconfig.json' was deleted, and 'package.json' and naming of certain files were updated accordingly. Also, FetchrInstance class continues to maintain the same functionality while having its properties and methods changed from TypeScript syntax to JavaScript syntax.

--- FetchrInstance.js
+++ FetchrInstance.js
@@ -1,32 +1,32 @@
-import fs from "fs-extra";
-import axios, {AxiosInstance} from "axios";
-import yaml from 'js-yaml';
+const axios = require("axios");
+const yaml = require("js-yaml");
+const fs = require("fs-extra");
 
 class FetchrInstance {
 
-    private api: AxiosInstance | undefined;
-    private filePath: string | undefined;
-    private file: any;
-    private cfg: any;
+    api;
+    filePath;
+    file;
+    cfg;
 
-    public constructor(filePath: string = "fetchr.yml") {
+    constructor(filePath = "fetchr.yml") {
         this.filePath = filePath;
         this.file = fs.readFileSync(filePath, "utf8");
         this.cfg = yaml.load(this.file);
         this.api = axios.create({ baseURL: this.cfg.config.baseURL })
     }
 
-    public routes() {
-        return Object.keys(this.cfg.routes).reduce((obj: any, routeName: any) => {
+    routes() {
+        return Object.keys(this.cfg.routes).reduce((obj, routeName) => {
             const routeConfig = this.cfg.routes[routeName];
 
-            obj[routeName] = async (...args: any) => {
+            obj[routeName] = async (...args) => {
                 let url = routeConfig.endpoint;
-                routeConfig.params.forEach((param: any, index: any) => {
+                routeConfig.params.forEach((param, index) => {
                     url = url.replace(`{${param}}`, args[index]);
                 });
 
-                if(this.api == undefined) return;
+                if(this.api === undefined) return;
 
                 const result = await this.api({
                     method: routeConfig.method,
