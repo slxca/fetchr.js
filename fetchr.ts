bc2cc4e75cfe1db918231ada1669ef921c590410
sluxa <straeter.luca@gmail.com>
2024-03-19 13:55:56 +0100

    Refactor fetchr class and update package dependencies
    The `fetchr.ts` file was refactored to improve flexibility and stability: unnecessary dynamic functions were removed, and the package was updated to use 'fs-extra' and 'axios'. The refactoring also included loading a configuration file, creating an axios instance for API calls, and defining individual API route handlers. Package dependencies have been updated in `package.json` and `package-lock.json`.

--- fetchr.ts
+++ fetchr.ts
@@ -1,20 +1,43 @@
+import fs from "fs-extra";
+import axios, {AxiosInstance} from "axios";
+import yaml from 'js-yaml';
 
 class fetchr {
 
-    func: any = {}
+    private api: AxiosInstance | undefined;
+    private filePath: string | undefined;
+    private file: any;
+    private cfg: any;
 
-    public fetchr() {
-        this.func["test"] = this.dynamicFunction("HelloWrld")
+    public fetchr(filePath: string = "fetchr.yml") {
+        this.filePath = filePath;
+        this.file = fs.readFileSync(filePath, "utf8");
+        this.cfg = yaml.load(this.file);
+        this.api = axios.create({ baseURL: this.cfg.config.baseURL })
     }
 
     public routes() {
-        return this.func;
-    }
+        return Object.keys(this.cfg.routes).reduce((obj: any, routeName: any) => {
+            const routeConfig = this.cfg.routes[routeName];
 
-    private dynamicFunction(...data: any) {
-        return function(hello: string = "") {
-            return data + hello;
-        }
+            obj[routeName] = async (...args: any) => {
+                let url = routeConfig.endpoint;
+                routeConfig.params.forEach((param: any, index: any) => {
+                    url = url.replace(`{${param}}`, args[index]);
+                });
+
+                if(this.api == undefined) return;
+
+                const result = await this.api({
+                    method: routeConfig.method,
+                    url: url
+                });
+
+                return result.data;
+            };
+
+            return obj;
+        }, {});
     }
 }
 
